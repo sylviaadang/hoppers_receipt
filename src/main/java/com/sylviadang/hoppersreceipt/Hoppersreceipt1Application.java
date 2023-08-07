e5e43789c8731ece812f9578a704edbc02447edf
Your Name <email@example.com>
2023-08-07 13:44:46 -0700

    finished

---
+++ src/main/java/com/sylviadang/hoppersreceipt/Hoppersreceipt1Application.java
@@ -0,0 +1,13 @@
+package com.sylviadang.hoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoppersreceipt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoppersreceipt1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylviadang/hoppersreceipt/Hoppersreceipt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviadang.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hoppersreceipt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
